8df64b467accc8b14c81533fbee6e1bb59d96849
fatih-tr <45336753+fatih-tr@users.noreply.github.com>
2020-02-06 17:44:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ return tf.tidy(()=>{
     //convert the image data to a tensor 
     let tensor = tf.fromPixels(img)
     //resize to 50 X 50
-    const resized = tf.image.resizeBilinear(tensor, [299, 299]).toFloat()
+    const resized = tf.image.resizeBilinear(tensor, [384, 504]).toFloat()
     // Normalize the image 
     const offset = tf.scalar(255.0);
     //const normalized = tf.scalar(1.0).sub(resized.div(offset));
